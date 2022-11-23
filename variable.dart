67af5c9e1a1addf0ed16c58e6be9f5eb31ead618
Shifat77 <85871622+Shifat77@users.noreply.github.com>
2022-11-23 20:59:08 +0600

    Create variable.dart

---
+++ variable.dart
@@ -0,0 +1,9 @@
+void main() {
+  String charecterName = "Stanly";
+  String charecterAge = "70";
+
+  print("Once is a man, named $charecterName");
+  print("He was $charecterAge years old");
+  print("He loves his name $charecterName");
+  print("But not being $charecterAge");
+}
